17b25643f0c110df28c7e01952fa3e36cc6b4afe
Daniel Alejandro Henao <danielhenao552@gmail.com>
2025-09-22 12:52:29 -0500

    corrected b-type managment

--- program.asm
+++ program.asm
@@ -1,11 +1,14 @@
 .data
 msg:    .asciiz "Hi"
+numd:   .dword 899
 num1:   .word 15
 num2:   .half 25
+space:  .space 6
 num3:   .byte 4
 
 .text
 main:
-    addi x1, x0, 5       # x1 = 5
-    addi x2, x0, 10      # x2 = 10
-    add  x3, x1, x2      # x3 = x1 + x2
+    addi x1, x0, 5
+    addi x2, x0, 10
+    add  x3, x1, x2
+    blt x3, x0, main
